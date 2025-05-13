84ef6ca945dd2233ee83e9d3342ad2b5bbb29258
AndreiRosu1712 <134957701+AndreiRosu1712@users.noreply.github.com>
2025-05-13 23:32:49 +0300

    Add files via upload

---
+++ Project-IP/src/main/java/com/example/Project_IP/dto/LoginResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.Project_IP.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String mesaj;
+    private String rol;
+}
