333e9f42507f77682259a5c8674fde808cf56a6e
AndreiRosu1712 <134957701+AndreiRosu1712@users.noreply.github.com>
2025-05-14 13:55:37 +0300

    Delete Project-IP directory

--- Project-IP/src/main/java/com/example/Project_IP/dto/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.Project_IP.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequestDto {
-    private String identifier; // poate fi username sau email
-    private String parola;
-}
